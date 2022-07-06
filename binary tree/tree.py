176bd45435cce50f7a52fc2c05bd4a3180b43d44
Kacper Sobczak <57370971+lyffski@users.noreply.github.com>
2022-07-06 09:18:56 +0200

    Update tree.py

--- binary tree/tree.py
+++ binary tree/tree.py
@@ -3,7 +3,6 @@
 # Leaf : Node without Children
 # Subtree : Grouping of Parent Children in the Main Tree
 
-# What is a Binary Tree
 # It is a tree thats nodes will only ever have 2 children and the
 # child on the left will have a value < than parent, while
 # the child on the right has a value > than the parent.
@@ -34,15 +33,14 @@ class BinaryTree:
             self.root = new_node
         else:
             # Set Root as the Node we will start with as
-            # we traverse the tree
+            # traverse the tree
             focus_node = self.root
 
             while True:
                 # Root is the top parent so we start there
                 parent = focus_node
 
-                # Check if new node should go on the left
-                # or right
+                # Check if new node should go on the left or right
                 if key < focus_node.key:
                     # Switch focus to the left child
                     focus_node = focus_node.left_child
@@ -67,7 +65,7 @@ class BinaryTree:
         # Start at the top
         focus_node = self.root # start traversing from root, thus focus_node
 
-        # While we haven't found it keep looking
+        # While program haven't found it keep looking
         while focus_node.key != key: #key the searched key
             if key < focus_node.key: #compering key with focus_node.key artribute
                 focus_node = focus_node.left_child  #if searched key smaller, than focus_node, if must be (if exist) in left subtrees, since only in left subtrees, may exist a key small then the parent/focus key
@@ -78,13 +76,11 @@ class BinaryTree:
                 return None
         return focus_node
 
-    # Now that we can add nodes it would be nice to traverse
-    # our Tree
     # In Order Traversal (SLIDE)
     # Start with left child, when None jump to Parent and to
     # the right child, back to parent and repeat
     def in_order_traverse(self, focus_node):
-        # We visit nodes in ascending order
+        # visit nodes in ascending order
         # Recursion is used to go to 1 node and then to its
         # children nodes
         if focus_node is not None:
@@ -94,8 +90,8 @@ class BinaryTree:
 
     # With Preorder Traversal we hit our focus node root
     # and then cycle through all our left children after
-    # that we jump up 1 parent and check for a right child
-    # Then we go back up to root and work our way down
+    # that it jump up 1 parent and check for a right child
+    # Then it go back up to root and work its way down
     # the right side.
     def preorder_traverse(self, focus_node):
         if focus_node is not None:
@@ -104,9 +100,9 @@ class BinaryTree:
             self.preorder_traverse(focus_node.right_child)
 
     # With Post Order Traversal our Root is
-    # going to come last. We start going to the
+    # going to come last. It start going to the
     # left child and then check both its children
-    # The we'll check Roots Right Child and its children
+    # The it check Roots Right Child and its children
     # finally checking Root last
     def postorder_traverse(self, focus_node):
         if focus_node is not None:
@@ -121,7 +117,7 @@ class Node: #class for creating a node, what is equipued in the __init__(), with
     def __init__(self, key=0, name=""):
         self.key = key
         self.name = name
-        # You can only have 1 left & right child
+        # You can only have 1 left & right child //tree prereqistion mandatory
         self.left_child = None
         self.right_child = None
 
@@ -138,7 +134,7 @@ tree.add_node(75, "Sales Manager")
 tree.add_node(85, "Salesman")
 
 # Test Different Ways to Traverse
-# As we traverse the Nodes the keys increase in order
+# As it traverse the Nodes the keys increase in order
 print(tree.in_order_traverse(tree.root))
 print(tree.preorder_traverse(tree.root))
 print(tree.postorder_traverse(tree.root))
